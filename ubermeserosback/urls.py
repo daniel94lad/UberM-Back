6fa8153ea6ff0d90f117abc25039b63a2cb9858b
Julio Fermin <julio_fermin@outlook.com>
2019-09-26 19:12:23 -0500

    adding the urls for profiles and events apis

--- ubermeserosback/urls.py
+++ ubermeserosback/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'profiles', views.ProfileViewSet)
+router.register(r'events', views.EventViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     #url(r'^api-auth/', include('rest_framework.urls'))
 
