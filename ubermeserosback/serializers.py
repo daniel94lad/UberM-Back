60ef73b0cbf07643e0d95d7b8c4b15824a1a700b
Julio Fermin <julio_fermin@outlook.com>
2019-10-05 18:19:39 -0500

    fixed profile post

--- ubermeserosback/serializers.py
+++ ubermeserosback/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from users.models import Profile
 from events.models import Event, EventAssistance
@@ -7,13 +8,22 @@ from postalcode.models import PostalCode
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     user = UserSerializer()
     class Meta:
         model = Profile
         fields = ['user','phoneNumber', 'avatar', 'active', 'streetAddress']
+
+    def create(self, validated_data):
+        #user = User.objects.get(pk=validated_data.pop('user'))
+        userData = validated_data.get('user')
+        userData['password'] = make_password(userData['password'])
+        userInstance = User.objects.create(**userData)
+        validated_data['user'] = userInstance
+        instance = Profile.objects.create(**validated_data)
+        return instance
 
 class PostalCodeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
